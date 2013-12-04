8ef9073227f8ade0d0555f67bdd6a66e7f727c18
Thomas Ongeri <ongerit@gmail.com>
2013-12-04 15:33:12 -0500

    new files

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+function showGra() {
+   document.getElementById('gra').style.display = "block";
+}
+
+function showUnd() {
+   document.getElementById('und').style.display = "block";
+}
+
+function showCer() {
+   document.getElementById('cer').style.display = "block";
+}
+function showCon() {
+   document.getElementById('con').style.display = "block";
+}
