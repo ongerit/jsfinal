7c20bef6a9e870fd3e585b5ad05d6f7e1c40c8eb
Thomas Ongeri <ongerit@gmail.com>
2013-12-04 17:52:47 -0500

    work at school

--- js/sam.js
+++ js/sam.js
@@ -1,14 +1,26 @@
 function showGra() {
    document.getElementById('gra').style.display = "block";
+   document.getElementById('und').style.height = "none";
+   document.getElementById('cer').style.display = "none";
+   document.getElementById('con').style.display = "none";
 }
 
 function showUnd() {
    document.getElementById('und').style.display = "block";
+   document.getElementById('gra').style.display = "none";
+   document.getElementById('cer').style.display = "none";
+   document.getElementById('con').style.display = "none";
 }
 
 function showCer() {
-   document.getElementById('cer').style.display = "block" & document.getElementById('con').style.display = "none";
+   document.getElementById('cer').style.display = "block";
+   document.getElementById('und').style.display = "none";
+   document.getElementById('gra').style.display = "none";
+   document.getElementById('con').style.display = "none";
 }
 function showCon() {
    document.getElementById('con').style.display = "block";
+   document.getElementById('und').style.display = "none";
+   document.getElementById('cer').style.display = "none";
+   document.getElementById('gra').style.display = "none";
 }
